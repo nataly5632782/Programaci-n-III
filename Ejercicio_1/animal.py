c380785d0900245bb68daf42df54e3c36ee09d9e
nataly5632782 <160240851+nataly5632782@users.noreply.github.com>
2024-08-08 11:54:17 -0500

    Tarea 08/08/24

---
+++ Ejercicio_1/animal.py
@@ -0,0 +1,7 @@
+class animal():
+    def __init__(self, nombre, peso):
+        self.nombre1 = nombre
+        self.peso1 = peso
+        
+
+         
---
+++ Ejercicio_1/ave.py
@@ -0,0 +1,25 @@
+from animal import animal 
+from datetime import datetime
+class ave (animal):
+     def __init__(self,nacimiento, propietario,nombre, peso):
+         
+         self.naci1= nacimiento 
+         self.prop= propietario 
+         super().__init__(nombre, peso)
+     
+     
+     def printdatos(self):
+       fecha2=datetime.now()
+       fecha3=(fecha2.year)
+       edad= fecha3-self.naci1
+       if(edad>5):
+           print("Es mayor de edad")
+       else:
+           print("Es menor de edad")
+       
+
+         
+         
+         
+x= ave(2005,"luca","gato",12)
+x.printdatos()
